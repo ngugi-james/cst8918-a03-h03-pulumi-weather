ed1c3d5203a8d778dde22b80d8d750731bf9dedf
safalafas <safal@ismt.edu.np>
2025-01-29 19:50:14 -0500

    Add cache module import in infrastructure index file

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,6 +3,7 @@ import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as docker from '@pulumi/docker'
 import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as cache from '@pulumi/azure-native/cache'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
